f03ac5c426466f2d6dc221ab2ad7e92e814c184f
Hesham8725 <heshamabdelbast87@gmail.com>
2023-04-24 11:19:19 +0200

    Add project files.

---
+++ EduZone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EduZone.Startup))]
+namespace EduZone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
