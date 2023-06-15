c819d25a49f8761dfb245fa52562aff930a011f1
3bda1137 <abdallahmahfouz111>
2023-06-15 22:08:07 +0300

    add models [PostInGroup,CommentForPostInTimeLine,PostInTimeLine . CommentForPostInGroup,LikeForPostInGroup,LikeForPostInTimeLine]

---
+++ EduZone/Models/LikeForPostInGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EduZone.Models
+{
+
+    [Table("LikeForPostInGroup")]
+    public class LikeForPostInGroup
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("PostInGroup")]
+        public int PostId { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserID { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public PostInGroup PostInGroup { get; set; }
+    }
+}
---
+++ EduZone/Models/LikeForPostInTimeLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EduZone.Models
+{
+    public class LikeForPostInTimeLine
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("PostInTimeLine")]
+        public int PostId { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserID { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public PostInTimeLine PostInTimeLine { get; set; }
+    }
+}
