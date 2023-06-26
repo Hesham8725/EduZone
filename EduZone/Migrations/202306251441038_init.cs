6ec50360d0f197b71b9b46d2e818f8bd4cf0586b
Mohamed <Mohamed.20353591@compit.aun.edu.eg>
2023-06-26 18:21:14 +0300

    All about course

---
+++ EduZone/Migrations/202306251441038_init.cs
@@ -0,0 +1,26 @@
+﻿namespace EduZone.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Course", "DepartmentId", "dbo.Department");
+            DropIndex("dbo.Course", new[] { "DepartmentId" });
+            AlterColumn("dbo.Course", "DepartmentId", c => c.Int());
+            CreateIndex("dbo.Course", "DepartmentId");
+            AddForeignKey("dbo.Course", "DepartmentId", "dbo.Department", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Course", "DepartmentId", "dbo.Department");
+            DropIndex("dbo.Course", new[] { "DepartmentId" });
+            AlterColumn("dbo.Course", "DepartmentId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Course", "DepartmentId");
+            AddForeignKey("dbo.Course", "DepartmentId", "dbo.Department", "Id", cascadeDelete: true);
+        }
+    }
+}
